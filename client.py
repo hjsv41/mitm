87b568a4ab7412b55fd07db8a857219c0403505b
hjsv41 <hjsv41@gmail.com>
2018-04-20 09:58:27 +0300

    added client

---
+++ client.py
@@ -0,0 +1,18 @@
+import socket as sk
+
+HOST = 'localhost'
+PORT = 2156
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+csock = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
+csock.connect(ADDR)
+
+while 1:
+    data = raw_input('... ')
+    csock.send(data)
+    data = csock.recv(1024)
+    if not data: break
+    print data
+
+csock.close()
